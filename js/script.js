1dbf4812a685de5f8667a6cba056615071367c09
Joshua Raichur <hello@joshuaraichur.com>
2015-02-10 14:00:03 +0530

    z-index

--- js/script.js
+++ js/script.js
@@ -12,22 +12,20 @@ $('#search').keyup(function() {
 			$('#sounds').html('');
 			var items = [];
 			$.each(data, function(key, val){
-				items.push("<div id='tracks_list'><a data-artist='"+val.user.username+"' data-title='"+val.title+ "' data-url=" + val.stream_url + " href='javascript:void();'><li><h2>"+val.title+"</h2><span class='plays'>" + val.playback_count+ " plays  -  <b>"+ val.user.username+  "</b></span></li></a>");
+				items.push("<div id='tracks_list'><a data-artist='"+val.user.username+"' data-title='"+val.title+ "' data-url=" + val.stream_url + " href='javascript:void();'><li><h2>"+val.title+"</h2><span class='plays'><b>"+ val.user.username+  "</b> " + val.playback_count+ " plays</span></li></a>");
 			});
 			$('#sounds').html(items.join(' '));
 			trackClick();
-
 		}
 	});
 
 });
 
-var clientid = 'client_id=0bcc7c4bcd2b5b55b23ab538c02f70c0';
-
 function trackClick(){
+	var clientid = 'client_id=0bcc7c4bcd2b5b55b23ab538c02f70c0';
 	$('#tracks_list a').click(function(){
-		var url= $(this).data('url') +"?"+ clientid;
-		var title= $(this).data('title');
+		var url = $(this).data('url') +".json?"+ clientid;
+		var title = $(this).data('title');
 		var artist = $(this).data('artist');
 		$(this).addClass('playedSong');
 		$('#navbar h2').html(title);
