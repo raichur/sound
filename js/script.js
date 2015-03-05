e0816feebb4f11957af3fc05d9d8291676453451
Joshua Raichur <hello@joshuaraichur.com>
2015-03-05 21:24:35 +0530

    fix button and add REAL instant feature

--- js/script.js
+++ js/script.js
@@ -1,45 +1,58 @@
 // Get list of sounds based on search input
-var delay = (function(){
-	var timer = 0;
-	return function(callback, ms){
-		clearTimeout (timer);
-		timer = setTimeout(callback, ms);
-	};
-})();
+var clientid = 'client_id=0bcc7c4bcd2b5b55b23ab538c02f70c0';
+var audioPlayer = document.getElementById('player');
+var soundName = "";
+var favSongs = ['Heroes', 'Ed Sheeran', 'Smle', 'Maroon 5', 'The Script', 'Coldplay', 'Skrillex', 'Deadmau5'];
+
+
+function getSongs() {
+	$.ajax({
+		url: "https://api.soundcloud.com/tracks.json?" + $('#search').serialize(),
+		dataType: 'json',
+		beforeSend:
+		function(data){
+			$('#sounds').empty();
+		},
+		success:
+		function(data){
+			$('#sounds').html('');
+			var items = [];
+			$.each(data, function(key, val){
+				items.push("<div id='tracks_list'><a data-artist='"+val.user.username+"' data-title='"+val.title+ "' data-url=" + val.stream_url + " href='javascript:void();'><li><h2>"+val.title+"</h2><span class='plays'><b>"+ val.user.username+  "</b> " + val.playback_count+ " plays</span></li></a>");
+			});
+			$('#sounds').html(items.join(' '));
+			trackClick();
+			if(audioPlayer.paused){
+				var url = $('#tracks_list:first-child a').data('url') + "?"+ clientid;
+				soundName = $('#tracks_list:first-child a').data('title');
+				var title = $('#tracks_list:first-child a').data('title');
+				var artist = $('#tracks_list:first-child a').data('artist');
+				$('#navbar h2').html(title);
+				audioPlayer.src = url;
+				audioPlayer.load();
+				document.getElementById('player').play();
+				return false;
+			}
+		}
+	});
+}
 
 $('#search').keyup(function() {
-	delay(function(){
-		$.ajax({
-			url: "https://api.soundcloud.com/tracks.json?" + $('#search').serialize(),
-			dataType: 'json',
-			beforeSend:
-			function(data){
-				$('#sounds').empty();
-			},
-			success:
-			function(data){
-				$('#sounds').html('');
-				var items = [];
-				$.each(data, function(key, val){
-					items.push("<div id='tracks_list'><a data-artist='"+val.user.username+"' data-title='"+val.title+ "' data-url=" + val.stream_url + " href='javascript:void();'><li><h2>"+val.title+"</h2><span class='plays'><b>"+ val.user.username+  "</b> " + val.playback_count+ " plays</span></li></a>");
-				});
-				$('#sounds').html(items.join(' '));
-				trackClick();
-			}
-		});
-	}, 500);
+	getSongs();
 });
 
-var audioPlayer = document.getElementById('player');
+$(function(){
+	$('#q').attr('value', favSongs[Math.floor(Math.random()*favSongs.length)]);
+	getSongs();
+});
 
 // Get sound from SoundCloud
 function trackClick(){
-	var clientid = 'client_id=0bcc7c4bcd2b5b55b23ab538c02f70c0';
 	$('#tracks_list a').click(function(){
 		var url = $(this).data('url') + "?"+ clientid;
 		var title = $(this).data('title');
+		soundName = $(this).data('title');
 		var artist = $(this).data('artist');
-		$(this).addClass('playedSong');
 		$('#navbar h2').html(title);
 		audioPlayer.src = url;
 		audioPlayer.load();
@@ -49,13 +62,13 @@ function trackClick(){
 }
 
 // Click events for control
-$('.playPauseToggle').click(function(e){
+$('#play').click(function(e){
 	e.preventDefault();
 	if(audioPlayer.paused){
 		audioPlayer.play();
-		$('.playPauseToggle').text("Pause");
+		$('#play').html('<svg width="2em"><use xlink:href="#icon-pause"/></svg>');
 	} else {
 		audioPlayer.pause();
-		$('.playPauseToggle').text("Play");
+		$('#play').html('<svg width="2em"><use xlink:href="#icon-play"/></svg>');
 	}
 });
